dcfc3102a0365622b269ca72b9f1115bed1d1e40
ding1020 <dingmeng10@sina.com>
2026-04-21 20:48:31 +0800

    Fix middleware: use authMiddleware

--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,9 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { authMiddleware } from "@clerk/nextjs";
 
-const isProtectedRoute = createRouteMatcher([
-  "/dashboard(.*)",
-  "/api/summarize(.*)",
-  "/api/documents(.*)",
-]);
-
-export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) {
-    auth().protect();
-  }
+export default authMiddleware({
+  publicRoutes: ["/", "/sign-in", "/sign-up", "/api/webhook"],
 });
 
 export const config = {
-  matcher: [
-    "/((?!.+.[w]+$|_next).*)",
-    "/(api|trpc)(.*)",
-  ],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
