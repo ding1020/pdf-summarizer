2b811ba6935d831faf5dd8dfe7b94614e8c9bb83
ding1020 <dingmeng10@sina.com>
2026-06-18 22:54:46 +0800

    feat: add Baidu auto-push JS for automatic indexing (no verification needed)

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -219,6 +219,13 @@ export default async function LocaleLayout({
             />
           )}
           <PwaRegister />
+          <Script
+            id="baidu-push"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `(function(){var bp=document.createElement('script');var curProtocol=window.location.protocol.split(':')[0];if(curProtocol==='https'){bp.src='https://zz.bdstatic.com/linksubmit/push.js';}else{bp.src='http://push.zhanzhang.baidu.com/push.js';}var s=document.getElementsByTagName("script")[0];s.parentNode.insertBefore(bp,s);})();`,
+            }}
+          />
           </ThemeProvider>
         </NextIntlClientProvider>
       </body>
